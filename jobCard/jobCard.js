c1d37a64d3f5ebaf737f76b0d7d7a068276e6e91
Vyshakh853 <vyshakh.e17@wipro.com>
2016-10-17 05:07:59 +0000

    Samarth sahayak webcomponents

--- jobCard/jobCard.js
+++ jobCard/jobCard.js
@@ -4,8 +4,9 @@ var currentScriptPath = scripts[scripts.length - 1].src;
 /*Defining the job Card web component*/
 angular.module('samarth-webcomponents')
     .component('jobCard', {
+        /*Binding jobID*/
         bindings: {
-            job: '<'
+            data: '<'
 
         },
         templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf(
@@ -15,6 +16,19 @@ angular.module('samarth-webcomponents')
     });
 
 /*Controller for job Card*/
-function jobCardController($scope) {
-    $scope.job = this.job;
+function jobCardController($scope, jobCardService) {
+    //$scope.job = this.job;
+    var jobID = this.data;
+    console.log("ID : " + this.data);
+    /*Calling the service for getting the details*/
+    jobCardService.getJobByID(jobID)
+        .then(function successCallback(response) {
+                console.log("Connected successfully" + response.data);
+                var job = response.data;
+                $scope.job = job[0];
+                //console.log("ID" + $scope.job.jobID);
+            },
+            function errorCallback(response) {
+                console.log("some error occured");
+            });
 }
