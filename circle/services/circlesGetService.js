b0372d3ef2f98c9ff2d083dbc5ed9be0ac108671
Akankshashri <akankshashri.2292@gmail.com>
2016-10-17 04:46:26 +0000

    circle component changed

--- circle/services/circlesGetService.js
+++ circle/services/circlesGetService.js
@@ -1,16 +1,34 @@
-angular.module('samarth-sahayak')
+angular.module('samarth-coordinator')
     .service('circlesGetService', function($http) {
-        return {
-            getCircle: function() {
-                return $http.get('/circle')
-                    .then(function(res) {
-                        console.log("got circles data");
-                        return res;
-                    }, function(res) {
-                        console.log(res);
-                        return;
-
-                    });
-            }
+        var objcircle = {};
+        //gets the circle from neo4j 
+        objcircle.getCircle = function() {
+            return $http.get('http://localhost:8081/circle/getcircle/:' + username)
+                .then(function(res) {
+                    //console.log("got circles data");
+                    return res;
+                }, function(error) {
+                    // console.log(res);
+                    return error;
+                });
         }
+
+        //adds the circle to mongodb and neo4j
+        objcircle.addCircle = function(circle) {
+            //console.log("service circle", circle);
+            return $http({
+                    url: "http://localhost:8081/circle/createcircle",
+                    method: "POST",
+                    data: circle
+                })
+                .then(function(response) {
+                        return response;
+                    },
+                    function(error) {
+                        return error;
+                    });
+
+
+        }
+        return objcircle;
     });
