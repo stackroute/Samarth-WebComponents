57bef56dd486d65a278d37ce92f4564dc6315f95
kishanlalbj <kishanlalbj@gmail.com>
2016-10-18 07:58:26 +0000

    Skill card modified to display skills as chips

--- skillcard/services/skillcardservice.js
+++ skillcard/services/skillcardservice.js
@@ -1,24 +1,24 @@
 angular.module('samarth-webcomponents')
-    .service('skillcardservice', function($http, $filter, $rootScope) {
-        return {
-            getskillcarddata: function(candidateid) {
-                var skillcarddata = {};
-                return $http({ 
-                    method: "get",
-                    url: "http://localhost:8081/skillcard/" + candidateid,
+.service('skillcardservice', function($http, $filter, $rootScope) {
+    return {
+        getskillcarddata: function(candidateid) {
+            var skillcarddata = {};
+            return $http({ 
+                method: "get",
+                url: "http://localhost:8081/skillcard/" + candidateid,
 
-                }).then(function mySucces(response)  { 
+            }).then(function mySucces(response)  { 
 
-                    var object = response.data.result;
+                var object = response.data.result;
 
-                    if (object.personalinfo[0].name != undefined) {
-                        skillcarddata['name'] = object.personalinfo[0].name;
-                    }
-                    if (object.personalinfo[0].dob != undefined) {
-                        skillcarddata['dob'] = $filter('date')(object.personalinfo[
-                            0].dob, "dd/MMM/yyyy");
-                        skillcarddata['age'] = (new Date()).getFullYear() - $filter(
-                            'date')(object.personalinfo[0].dob, "yyyy");
+                if (object.personalinfo[0].name != undefined) {
+                    skillcarddata['name'] = object.personalinfo[0].name;
+                }
+                if (object.personalinfo[0].dob != undefined) {
+                    skillcarddata['dob'] = $filter('date')(object.personalinfo[
+                        0].dob, "dd/MMM/yyyy");
+                    skillcarddata['age'] = (new Date()).getFullYear() - $filter(
+                        'date')(object.personalinfo[0].dob, "yyyy");
                     }
                     if (object.personalinfo[0].gender != undefined) {
                         skillcarddata['gender'] = object.personalinfo[0].gender;
@@ -34,14 +34,14 @@ angular.module('samarth-webcomponents')
                     }
                     if (object.workexp[0].workexperience.length > 0) {
                         skillcarddata['location'] = object.workexp[0].workexperience[
-                            0].Location;
+                        0].Location;
                     }
                     if (object.workexp[0].workexperience.length > 0) {
                         skillcarddata['designation'] = object.workexp[0].workexperience[
-                            0].designation;
+                        0].designation;
                     }
                     if (object.skill[0].skills.length > 0) {
-                        skillcarddata['skills'] = [(object.skill[0].skills)];
+                        skillcarddata['skills'] = object.skill[0].skills;
                     }
 
                     // skillcarddata['name']=object.personalinfo[0].name;
@@ -51,7 +51,7 @@ angular.module('samarth-webcomponents')
                 }, function myError(response) { 
                     console.log("error in getting skillcard details"); 
                 });
-            }
+        }
 
-        };
-    });
+    };
+});
