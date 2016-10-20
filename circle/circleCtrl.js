e6e5fe28a10c43cecea9f1a3c25ed7173028d9b1
Akankshashri <akankshashri.2292@gmail.com>
2016-10-20 06:42:23 +0000

    Circle Updated

--- circle/circleCtrl.js
+++ circle/circleCtrl.js
@@ -5,57 +5,17 @@ angular.module('samarth-webcomponents')
     .component('myCircle', {            
         templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf(
             '/')) + '/templates/circles.html',
-        controller: circleCrtl
+        controller: circleCrtl,
+        bindings: {
+            prof: '<'
+        }
     });
 // Ctrl for circle component
-function circleCrtl($http, $log, $mdDialog, circlesGetService) {
-    console.log("enter in controller");
-    var ctrl = this;
-    // ctrl.circle.profilePic = "http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/artheader.jpg";
-
-    circlesGetService.getCircle()
-        .then(function(response) {
-            ctrl.profiling = response.data;
-
-            // console.log(ctrl.profiling.rCount);
- // console.log(ctrl.profiling.name);
-
-        }, function(err) {
-
-        });
-
-    ctrl.showForm = function(ev) {
-        $mdDialog.show({
-                templateUrl: 'webcomponents/circle/templates/addCircle.html',
-                controller: 'dialogController',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-            })
-            .then(function(circle) {
-                //    console.log(circle);
-                circle.profilePic = "http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/artheader.jpg";
-                //  console.log(circle);
-                circlesGetService.addCircle(circle);
-            }).then(function success(response) {
-                //alert("circle added");
-            }, function(err) {
-
-            });
-    };
+function circleCrtl($http, $log) {
+    $log.log("enter in component");
 
 }
-//adds the circle to neo4j
-function dialogController($scope, $mdDialog, circlesGetService) {
-    $scope.cancel = function() {
-        $mdDialog.cancel();
-    }
-    $scope.submit = function(circle) {
-        console.log(circle);
-        $mdDialog.hide(circle);
-    }
 
-}
 
 // angular.module('samarth-webcomponents')
 //     .controller('dialogController', function() {
