051e37e76de142b30b62159d20f058384fb8c0d7
Vyshakh853 <vyshakh.e17@wipro.com>
2016-10-21 10:52:12 +0000

    Samarth webcomponents

--- jobcard/jobcard.js
+++ jobcard/jobcard.js
@@ -32,4 +32,33 @@ function jobcardCtrl($scope, jobCardService) {
             function errorCallback(response) {
                 console.log("some error occured");
             });
+
+    function createDownloadUrl() {
+        name = $scope.job.jobID + ".png";
+        //ctrl.data1 = ctrl.data;            
+        $scope.downloaddata = JSON.stringify($scope.job);
+
+        blob = new Blob([$scope.downloaddata], {
+                type: 'text/plain'
+            }),
+            url = $window.URL || $window.webkitURL;
+        ctrl.fileUrl = url.createObjectURL(blob);
+    }
+    var getCanvas;
+    $scope.render = function(ev) {
+        var card = angular.element(document.querySelector('#totalcardarea'));
+        html2canvas(card, {
+            onrendered: function(canvas) {
+                getCanvas = canvas;
+                $scope.downloadCard();
+            }
+        });
+    }
+    $scope.downloadCard = function() {
+        var imageData = getCanvas.toDataURL("image/png");
+        var newData = imageData.replace(/^data:image\/png/,
+            "data:application/octet-stream");
+        var download = angular.element(document.querySelector('#download'));
+        download.attr("download", name).attr("href", newData);
+    }
 }
