be513f8480c38409bac6d2c8ceed5f2f10dde285
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-11-05 13:11:38 +0530

    Removing lintfixes folder, it is not needed in repo, will be adding it to gitignore

--- lintfixes/services/verificationbadgeservice.js
+++
@@ -1,8 +0,0 @@
-angular.module('samarth-webcomponents')
-    .service('verificationbadgeService', ['$http', function($http) {
-        return {
-            getbadgedata: function(candidateid) {
-                return $http.get('/verification/' + candidateid);
-            }
-        };
-    }]);
