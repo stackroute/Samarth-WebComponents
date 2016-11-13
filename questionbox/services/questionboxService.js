43ed286c6ce5c914f390b6d4c13594e050edc273
Navandeep025 <navandeepkaur25@gmail.com>
2016-11-13 12:15:13 +0000

    Removal of comments

--- questionbox/services/questionboxService.js
+++ questionbox/services/questionboxService.js
@@ -2,7 +2,6 @@ angular.module('samarth-webcomponents')
     .service('quesnboxService', function($http, $rootScope,
          $auth) {
         var candidateid = $auth.getPayload().uname;
-       // var candidateid = 7204487502;
 
         return {
             questionGenerator: function(lang) {
@@ -11,30 +10,23 @@ angular.module('samarth-webcomponents')
                     'project',
                     'skills',
                     'qualification'
-
-
                 ];
-                // var randomNumber = 1;
+                
                 var randomNumber = Math.floor(Math.random() * sectionArray.length);
-                // console.log("Section array....", sectionArray[randomNumber])
+                
                 return $http({
                     method: 'GET',
-                    // url: 'http://localhost:8081/candidates/' 
-      // url: 'http://localhost:8081/candidates/' + candidateid +
-      //                   '/qboxquestions?sections=' + sectionArray[randomNumber] +
-      //                   '&limit=2&skip=0&lang=English' 
-                  url: 'http://localhost:8081/candidates/' + candidateid +
+                    url: 'http://localhost:8081/candidates/' + candidateid +
                         '/qboxquestions?sections=' + "skills" +
                         '&limit=2&skip=0&lang=English'         
-                }).then(function successCallback(response) {
-                    var questionObj = response.data;
-                    console.log("About to save questionObj", questionObj);
-                    return questionObj;
-                }, function errorCallback(response) {
-                    console.log('Error accord during Project Section')
-                    return;
-                });  
-
+                        }).then(function successCallback(response) {
+                            var questionObj = response.data;
+                            console.log("About to save questionObj", questionObj);
+                            return questionObj;
+                        }, function errorCallback(response) {
+                            console.log('Error accord during Project Section')
+                            return;
+                        });  
             },
             updatequestion: function(questiondata, answer) {
                 return $http({
--- rubric/rubriccomponent.js
+++ rubric/rubriccomponent.js
@@ -3,16 +3,12 @@
 
     let scripts = document.getElementsByTagName('script');
     let currentScriptPath = scripts[scripts.length - 1].src;
-
- //   var path = currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/')) + '/template/notification.html';  //wave 9
-
     angular.module('samarth-webcomponents')
         .component('smVerificationRubric', {
             bindings: {
                 name: '<',
                 outcome: '=',
                 remarks: '='
-
             },
             templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf(
                 '/')) + '/template/rubric.html',
@@ -30,7 +26,6 @@
 
         rubricservice.getrubricdata(ctrl.name)
             .then(function(response) {
-                // console.log("inside rubricCtrl", response.data);
                 ctrl.description.push(response.data);
 
                 ctrl.desc = ctrl.description[0];
@@ -38,14 +33,9 @@
                 ctrl.desc1 = ctrl.desc[0];
                 ctrl.name = ctrl.desc1.type.name[0];   //wave 9
 
-                // console.log(ctrl.desc1);
-                // console.log(ctrl.desc1.scale);
             }, function(err) {
                 console.log(err);
             });
-
-        // console.log(ctrl.outcome);
-
 
         ctrl.average = function(desc) {
             angular.forEach(desc, function(value, key) {
