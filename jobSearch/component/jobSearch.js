760fe3112f1e373077b2406fefce012a2f616766
Ankit Vashist <ankitvashist11@gmail.com>
2016-12-09 05:44:19 +0000

    updating job search from json seraching to api data fetching

--- jobSearch/component/jobSearch.js
+++ jobSearch/component/jobSearch.js
@@ -1,18 +1,33 @@
 (function(){
   'use strict'
 angular
-  .module('samarth-webcomponents')
-  /*creating component for job search*/
-  .component("jobSearch",{
-    templateUrl:'./samarth-webcomponents/jobSearch/template/jobSearch.html',
-    controller:'jobSearchCtrl'
-    /*bindings:{
-      name :"=",
-      txt:'@'
-    },
-    controller:function()
-    {
-    
-    }*/
-  })
+  .module("samarth-webcomponents")
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('index.jobSearch', {
+      url:'jobsearch',
+      views:{
+        'content@': {
+          templateUrl: './samarth-webcomponents/jobSearch/template/jobSearchIndex.html'
+        }
+      }
+    })
+   /* .state('index.jobSearchresult', {
+      url:'jobsearch/:searchstr',
+      views:{
+        'content@': {
+          templateUrl: './samarth-webcomponents/jobSearch/template/jobSearchIndex.html',
+          controller: jobsearchresultctrl
+        }
+      }
+    })*/
+  });
 })();
+
+
+/*jobsearchresultctrl($stateParams) {
+  var searchQueryStr = $stateParams.searchstr;
+  service.searchJobs(searchQueryStr).then()
+}
+*/
--- jobSearch/controller/jobSearchCtrl.js
+++ jobSearch/controller/jobSearchCtrl.js
@@ -8,28 +8,28 @@ angular
 		    vm.title="";
 
 		  	$scope.searchfun=function(key){
+
 		  	vm.arrayspace=[];
 		  	vm.arrayLength=0;
 
 		  	jobSearchFactory.searchJobDetails().then(function(response){
-				//console.log(searchString);
-				vm.result = response.data.Search;
+				//1st line is for my json 
+				//vm.result = response.data.Search;
+				vm.result=response.data;
 				//console.log($scope.result);
 				vm.arrayLength=vm.result.length;
-				//var i=0;
-				//var arrayspace=[];
+				
 	      for(var i=0;i<vm.arrayLength;i++){
-	      	//console.log("for");
-			 	vm.title=vm.result[i].title;
-				if(vm.title==$scope.searchString)
-				{	/*console.log("b4 push");*/
-		     vm.arrayspace.push(vm.result[i]);
-		     //console.log("after push");
-				 $rootScope.$broadcast('san',vm.arrayspace);
-				}
-				else {
-				 	// console.log("no");
-				  }
+
+				 	vm.title=vm.result[i].title;
+					if(vm.title==$scope.searchString)
+					{	
+			     vm.arrayspace.push(vm.result[i]);
+					 $rootScope.$broadcast('san',vm.arrayspace);
+					}
+					else{
+					 	
+					}
 			 	}
 				},
 				function (err)
