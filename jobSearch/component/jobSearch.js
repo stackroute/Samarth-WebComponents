3c3429f4eaefdce9805ae0d5f2559f8e3598d899
Ankit Vashist <ankitvashist11@gmail.com>
2016-12-09 07:37:28 +0000

    updating file

--- jobSearch/component/jobSearch.js
+++ jobSearch/component/jobSearch.js
@@ -1,33 +1,18 @@
 (function(){
   'use strict'
 angular
-  .module("samarth-webcomponents")
-  .config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-    .state('index.jobSearch', {
-      url:'jobsearch',
-      views:{
-        'content@': {
-          templateUrl: './samarth-webcomponents/jobSearch/template/jobSearchIndex.html'
-        }
-      }
-    })
-   /* .state('index.jobSearchresult', {
-      url:'jobsearch/:searchstr',
-      views:{
-        'content@': {
-          templateUrl: './samarth-webcomponents/jobSearch/template/jobSearchIndex.html',
-          controller: jobsearchresultctrl
-        }
-      }
-    })*/
-  });
+  .module('samarth-webcomponents')
+  /*creating component for job search*/
+  .component("jobSearch",{
+    templateUrl:'./samarth-webcomponents/jobSearch/template/jobSearch.html',
+    controller:'jobSearchCtrl'
+    /*bindings:{
+      name :"=",
+      txt:'@'
+    },
+    controller:function()
+    {
+    
+    }*/
+  })
 })();
-
-
-/*jobsearchresultctrl($stateParams) {
-  var searchQueryStr = $stateParams.searchstr;
-  service.searchJobs(searchQueryStr).then()
-}
-*/
