0622cbf8466e061d635114d05d2e9c860959dba8
Amol Tiwari <amoltiwari.16@gmail.com>
2016-12-12 11:02:22 +0000

    jobprovider jobcode functionality updated

--- jobproviderreg/component/jobProvider.js
+++ jobproviderreg/component/jobProvider.js
@@ -19,45 +19,33 @@
                 vm.jobprovider.jpCode="";
                 vm.subjobprovider = subjobprovider;
                $scope.$watch('vm.jobprovider.jpCode', function(newValue, oldValue) {
-                 // do stuff to the text everytime it chan
-               
                  if(newValue.length===5){
                   jobproviderfactory.jpCodeCheck(vm.jobprovider.jpCode).then(function(response){
                     if(response.data.count>=1){
-                    // $scope.myForm.jpCode.$setValidity(false);
-                    // console.log("in code "+response.data.count);
-                    // vm.msg="Some error occurred";
-                    // console.log(response.data.count+" >1");
                     vm.availability="Not available";
                     }if(response.data.count==0){
                     // console.log(response.data.count+" =0");
                     vm.availability="Available";
                   }
                   }),function(err){
-                    console.log("error in code check")
+                    vm.msg="Some error occured!!";
                   }
                  }else{
                     vm.availability="disabled";
                  }
                });
                 function subjobprovider(){
-                  console.log(vm.jobprovider);
-                  jobproviderfactory.jobproviderdata(vm.jobprovider).then(function(response){
-                    // if(response.body.error) {
-                      //Jobprovider code is taken up already
-                      // console.log("Data post success");
-                      // this.error = response.body.error;
-                    // } else {
-                      //success posted
-                      vm.msg="Registered successfully";
-
-                    
-                     
+                  if(vm.availability=="Available"){
+                    jobproviderfactory.jobproviderdata(vm.jobprovider).then(function(response){
+                  vm.msg="Registered successfully";
                   }),function(err){
-                     console.log('Error in data post');
+                     vm.msg='Some error occurred';
                   }
+                }else{
+                  vm.msg="Please try with some other jobprovider code!!";
                 }
+                  }
+            
               }
-
       }
 )();
