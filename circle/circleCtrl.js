70ebd357983c6040141cac5ed3c72d1d444a29ae
PrernaKukreti <kukretiprerna05@gmail.com>
2016-12-27 11:42:44 +0000

    alignment issues fixed

--- circle/circleCtrl.js
+++ circle/circleCtrl.js
@@ -1,31 +1,23 @@
-var scripts = document.getElementsByTagName("script");
-var currentScriptPath = scripts[scripts.length - 1].src;
+(function() {
 
-angular.module('samarth-webcomponents')
-    .component('myCircle', {            
-        templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf(
-            '/')) + '/templates/circles.html',
-        controller: circleCrtl,
-        bindings: {
-            data: '<'
-        }
-    });
-// Ctrl for circle component
-function circleCrtl($http, $log, $scope, $mdColorPalette) {
-    console.log("data : " + this.data);
-    $scope.prof = this.data;
-    var colors = Object.keys($mdColorPalette);
 
-    $scope.gerColler = function(a) {
-        console.log(a.length)
-        return colors[a.length];
+    angular.module('samarth-webcomponents')
+        .component('circle', {
+            templateUrl:  './samarth-webcomponents/circle/templates/circles.html',
+            controller: circleCtrl,
+            bindings: {
+                data: '<'
+            }
+        });
+
+    // Ctrl for circle component
+    function circleCtrl($http, $log, $scope, $mdColorPalette) {
+        console.log('data : ');
+        $scope.prof = this.data;
+        console.log($scope.prof);
+        let colors = Object.keys($mdColorPalette);
+        $scope.gerColler = function(a) {
+            return colors[a.length];
+        };
     }
-}
-
-
-
-// angular.module('samarth-webcomponents')
-//     .controller('dialogController', function() {
-
-
-//     });
+})();
