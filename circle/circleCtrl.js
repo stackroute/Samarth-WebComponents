e319e2b485caae36a1938c66c7187a9aa2948006
shinderpalsingh <shinders1993@gmail.com>
2016-12-30 06:10:16 +0000

    add transclude

--- circle/circleCtrl.js
+++ circle/circleCtrl.js
@@ -1,4 +1,33 @@
 
+// (function() {
+
+
+//    angular.module('samarth-webcomponents')
+//        .component('circle', {
+//            templateUrl:  './samarth-webcomponents/circle/templates/circles.html',
+//            controller: circleCtrl,
+//            bindings: {
+//                data: '<'
+//            },
+//            transclude:
+//            {
+//                circleStats:'circleStats'
+//            }
+//        });
+
+//    // Ctrl for circle component
+//    function circleCtrl($http, $log, $scope, $mdColorPalette) {
+//        console.log('data : ');
+//        $scope.prof = this.data;
+//        console.log($scope.prof);
+//        let colors = Object.keys($mdColorPalette);
+//        $scope.gerColler = function(a) {
+//            return colors[a.length];
+//        };
+//    }
+// })();
+
+
 (function() {
 
 
@@ -8,7 +37,11 @@
             controller: circleCtrl,
             bindings: {
                 data: '<'
-            }
+            },
+                       transclude:
+           {
+               circleStats:'circleStats'
+           }
         });
 
     // Ctrl for circle component
@@ -22,4 +55,3 @@
         };
     }
 })();
-
