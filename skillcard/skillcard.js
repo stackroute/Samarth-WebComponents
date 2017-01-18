e0d781db7bf8bd72134c1ff5421a68510efadc7e
somnath111993 <somnath.jan1@gmail.com>
2017-01-18 10:01:56 +0530

    file upload integration in skillcard

--- skillcard/skillcard.js
+++ skillcard/skillcard.js
@@ -21,10 +21,11 @@
                 }
         });
 
-        function skillcardCtrl($window, $timeout, $mdDialog, skillcardService, $rootScope, $state, $http) {
+        function skillcardCtrl($window, $timeout, $mdDialog, Upload, skillcardService, $rootScope, $state, $http) {
             let ctrl = this;
             let name;
             console.log("entered in to controller of skillcard");
+
             console.log($rootScope);
             if($rootScope.pre=="dashboard")
             {
@@ -51,13 +52,69 @@
                 });
 
             let showEditForm = false ;
+            let newPicURL = '';
+            let candidateID = this.candidateid; 
 
-            this.edit = function(newUrl){
-                console.log("newURl ---->",newUrl);
+            closeEditForm = function () {
+                /* body... */
+                this.showEditForm =false;
+            }
+
+            this.uploadFiles = function (files) {
+                this.Files = files;
+
+                // async.series([
+                //     function(done) {
+                //         console.log('ConnectAWS function')
+                //         done()
+                //     },
+                //       function() {
+                //         console.log('second thing')
+                        
+                //         // done(new Error('another thing'))
+                //       }
+
+                // ])
+                // this.amazonBucket = [];
+                // skillcardService.ConnectAWS().then(function (result) {
+                //     /* body... */
+                //     // amazonBucket = result;
+                //     console.log('Connected to storage server!!!');
+                // }, function (error) {
+                //     /* body... */
+                //     console.log('Error while connecting to storage server!!!');
+                // });
+
+                if (files && files.length > 0) {
+                    angular.forEach(this.Files, function (file, key) {
+                        skillcardService.Upload(file).then(function (result) {
+                            // Mark as success
+                            file.Success = true;
+                            newPicURL = result.Location;
+                            console.log('newPicURL: '+ newPicURL);
+                            skillcardService.uploadPicUrl(newPicURL,candidateID);
+                            closeEditForm();
+                        }, function (error) {
+                            // Mark the error
+                            this.Error = error;
+                            this.showEditForm =false;
+                            alert('some error occured while uploading the pic, Please try after sometime!');
+
+                        }, function (progress) {
+                            // Write the progress as a percentage
+                            file.Progress = (progress.loaded / progress.total) * 100
+                        });
+                    });
+                }
+            }
+
+            this.edit = function(){
+                // console.log("newURl ---->",newUrl);
                 if(this.showEditForm){
-                    skillcardService.uploadPicUrl(newUrl,this.candidateid);
-                    ctrl.data.profilepic = newUrl;
-                    console.log("uploaded");
+
+                    // skillcardService.uploadPicUrl(newUrl,this.candidateid);
+                    // ctrl.data.profilepic = newUrl;
+                    console.log("upload form open");
 
                     this.showEditForm =false;
                 }else{
@@ -110,22 +167,58 @@
             $timeout(createDownloadUrl, 1000);
 
             ctrl.render = function(ev) {
-                let card = angular.element(document.querySelector('#totalcardarea'));
-                html2canvas(card, {
+
+                // html2canvas(document.getElementById('totalcardarea').innerHTML, {
+                //     useCORS: true,
+                //     onrendered: function (canvas) {
+                //     var imgval = canvas.toDataURL("image/png");
+                //     // document.getElementById("HiddenField1").value = dataUrl;
+                //                      imgval = imgval.Replace("data:image/png;base64,", "");
+                //     byte[] imgData = Convert.FromBase64String(imgval);
+
+                //     using (System.Drawing.Image image = System.Drawing.Image.FromStream(new MemoryStream(imgData)))
+                //     {
+                //         String path = Server.MapPath("~/imgFolder");
+                //          image.Save(path + "\\output.jpg", ImageFormat.Jpeg);  // Or Png
+
+                //     }
+                //     }
+                // });
+                let card = document.querySelector('#totalcardarea');
+                console.log(card);
+                
+                // let card = angular.element(document.querySelector('#totalcardarea'));
+                // let card = document.getElementById('totalcardarea').innerHTML;
+                html2canvas(card, { allowTaint: true,
+                    useCORS: true,
+                    logging: true,
                     onrendered: function(canvas) {
-                        getCanvas = canvas;
-                        ctrl.downloadcard();
+                        // getCanvas = canvas;
+                        // card.appendChild(getCanvas);
+                        // ctrl.downloadcard(canvas);
                         // ctrl.showConfirm(ev);
+                        // var myImage = canvas.toDataURL("image/png");
+                        // window.open(canvas.toDataURL("image/jpeg"));
+
+                        let anchor = angular.element(document.querySelector('#download'));
+                        var imgageData = canvas.toDataURL("image/png");
+                        var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+                        anchor.attr("download", "your_pic_name.png").attr("href", newData);
                     }
-                });
+                    });
+                    
+                // });
             };
 
-            ctrl.downloadcard = function() {
-                let imgageData = getCanvas.toDataURL('image/png');
-                let newData = imgageData.replace(/^data:image\/png/,
-                    'data:application/octet-stream');
-                let dwld = angular.element(document.querySelector('#download'));
-                dwld.attr('download', name).attr('href', newData);
+            ctrl.downloadcard = function(card) {
+                let imgageData = card.toDataURL('image/png');
+                console.log(imgageData);
+                // let newData = imgageData.replace(/^data:image\/png/,
+                //     'data:application/octet-stream');
+                // console.log(newData);
+                // let dwld = angular.element(document.querySelector('#download'));
+                // console.log(dwld);
+                dwld.attr('download', name).attr('href', imgageData);
             };
         }
 })();
